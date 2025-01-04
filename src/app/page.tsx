64dcdf8cd9fd20297bb3b6703bc8f43dfb18d093
duythong114 <tranthong.facebook@gmail.com>
2025-01-04 16:05:35 +0700

    using image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-thin text-center">Xin Chào Duy Thống</h1>
+      <div className="w-[700px] h-[700px] bg-red-300">
+        <Image
+          src="/images/music.jpg"
+          alt="music"
+          width={500}
+          height={500}
+          quality={100}
+          className="w-[500px] h-[500px] object-cover"
+        />
+      </div>
     </main>
   );
 }
